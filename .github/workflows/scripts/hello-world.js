5b8c19f61fbd81e2bdd988cad22e69f72c63b42c
michel <jsnerdic@gmail.com>
2024-08-27 14:30:02 +0200

    test

--- .github/workflows/scripts/hello-world.js
+++ .github/workflows/scripts/hello-world.js
@@ -8,16 +8,21 @@ export default async function ({ core, github, context }) {
 		const owner = context.repo.owner;
 		const repo = context.repo.repo;
 		const issueNumber = context.issue.number;
+		const inputs = github.event.inputs
+
+		core.info(`>>> inputs: ${JSON.stringify(inputs)}`)
 
 		core.info(`>>> owner: ${owner}`);
 		core.info(`>>> repo: ${repo}`);
 		core.info(`>>> issue number: ${issueNumber}`);
 
-		await github.rest.issues.addLabels({
+		core.info(`>>> issue closing reason: ${context.issue.state_reason}`)
+
+		await github.rest.issues.addComment({
 			owner,
 			repo,
 			issue_number: issueNumber,
-			labels: ['documentation'],
+			labels: 'Hello there! :D',
 		});
 	} catch (error) {
 		core.setFailed(error.message);
