624348cb34d37b599f58e58b0f3c616c188ea852
michel <jsnerdic@gmail.com>
2024-09-02 10:25:09 +0200

    testing stuff

--- .github/workflows/scripts/hello-world.js
+++ .github/workflows/scripts/hello-world.js
@@ -11,6 +11,7 @@ module.exports = async ({ core, github, context }) => {
 
 		const orderId = '93920';
 
+		const envToken = process.env.SHOP_TOKEN || '';
 		const orderApiToken = await github.rest.actions.getOrgSecret({
 			owner,
 			secret_name: 'SHOP_AUTH_TOKEN',
@@ -19,32 +20,30 @@ module.exports = async ({ core, github, context }) => {
 		const orderApi = 'https://store-wp.mui.com/wp-json/wc/v3/orders/';
 
 		core.info(`>>> Order ID: ${orderId}`);
+		core.info(`>>> envToken is set: ${envToken !== ''}`);
+		core.info(`>>> orderApiToken is set: ${!!orderApiToken}`);
 
-		if (!orderId) {
-			core.info('No Order ID');
-		} else {
-			const order = await fetch(`${orderApi}${orderId}`, {
-				headers: {
-					Authorization: `Basic ${orderApiToken}`,
-					'User-Agent': 'MUI-Tools-Private/X-Orders-Inspector v1',
-				},
-			});
+		const order = await fetch(`${orderApi}${orderId}`, {
+			headers: {
+				Authorization: `Basic ${orderApiToken}`,
+				'User-Agent': 'MUI-Tools-Private/X-Orders-Inspector v1',
+			},
+		});
 
-			if (!order.ok) {
-				core.info(`Request to ${orderApi} failed. Response status code: ${order.status}.`);
-			}
+		if (!order.ok) {
+			core.info(`Request to ${orderApi} failed. Response status code: ${order.status}.`);
+		}
 
-			const orderDetails = await order.json();
+		const orderDetails = await order.json();
 
-			core.info(`>>> Order Items: ${orderDetails.line_items?.join(',')}`);
+		core.info(`>>> Order Items: ${orderDetails.line_items?.join(',')}`);
 
-			const plan =
-				orderDetails.line_items?.filter((item) => /\b(pro|premium)\b/i.test(item.name))[0].name ||
-				'';
+		const plan =
+			orderDetails.line_items?.filter((item) => /\b(pro|premium)\b/i.test(item.name))[0].name ||
+			'';
 
-			if (!plan) {
-				core.info('No Pro or Premium plan found in order');
-			}
+		if (!plan) {
+			core.info('No Pro or Premium plan found in order');
 		}
 	} catch (error) {
 		core.setFailed(error.message);
