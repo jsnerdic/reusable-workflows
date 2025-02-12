341d587f23a450a16a31376c358df193f479f9f5
michel <jsnerdic@gmail.com>
2025-02-12 12:11:45 +0100

    test workflows

--- .github/workflows/scripts/hello-world.js
+++ .github/workflows/scripts/hello-world.js
@@ -7,8 +7,20 @@
  */
 module.exports = async ({ core, context, github }) => {
 	try {
-		core.setOutput('TEST_OUT1', ['a1', 'b1']);
-		core.setOutput('TEST_OUT2', ['a2', 'b2']);
+		const owner = context.repo.owner;
+		const repo = context.repo.repo;
+		const issueNumber = context.issue.number;
+
+		const issue = await github.rest.issues.get({
+			owner,
+			repo,
+			issue_number: issueNumber,
+		});
+
+		core.info(`>>> Issue fetched: ${issue.data.number}`);
+		core.info(`>>> Issue title: ${issue.data.title}`);
+		core.info(`>>> Issue state: ${issue.data.state}`);
+		core.info(`>>> Issue state reason: ${issue.data.state_reason}`);
 	} catch (error) {
 		core.error(`>>> Workflow failed with: ${error.message}`);
 		core.setFailed(error.message);
