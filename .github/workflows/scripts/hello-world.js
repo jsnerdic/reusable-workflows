b21424de1b6c55ae88b4f71dff7c94e130401a21
michel <jsnerdic@gmail.com>
2025-03-11 10:10:09 +0100

    testing pr workflow

--- .github/workflows/scripts/hello-world.js
+++ .github/workflows/scripts/hello-world.js
@@ -1,4 +1,26 @@
 // @ts-check
+const createEnumerationFromArray = (stringArray) =>
+	stringArray.length > 1
+		? stringArray
+			.slice(0, -1)
+			.map((s) => `\`${s}\``)
+			.join(', ') +
+		' or ' +
+		`\`${stringArray.slice(-1)}\``
+		: stringArray.map((s) => `\`${s}\``).join('');
+
+const typeLabels = [
+	'docs',
+	'release',
+	'bug',
+	'regression',
+	'maintenance',
+	'dependencies',
+	'enhancement',
+	'new feature',
+];
+const labelRegex = new RegExp(`\\b(${typeLabels.join('|')})\\b`, 'i');
+
 /**
  * @param {Object} params
  * @param {import("@actions/core")} params.core
@@ -9,22 +31,75 @@ module.exports = async ({ core, context, github }) => {
 	try {
 		const owner = context.repo.owner;
 		const repo = context.repo.repo;
-		const issueNumber = context.issue.number;
+		const pullNumber = context.issue.number;
 
-		core.info(`>>> Context: issue number: ${issueNumber}`);
-
-		const issue = await github.rest.issues.get({
+		const { data: pr } = await github.rest.pulls.get({
 			owner,
 			repo,
-			issue_number: issueNumber,
+			pull_number: pullNumber,
 		});
 
-		core.info(`>>> Fetch: Issue number: ${issue.data.number}`);
-		core.info(`>>> Fetch: Issue title: ${issue.data.title}`);
-		core.info(`>>> Fetch: Issue state: ${issue.data.state}`);
-		core.info(`>>> Fetch: Issue state reason: ${issue.data.state_reason}`);
+		core.info(`>>> PR fetched: ${pr.number}`);
 
-		core.info(`>>> Fetch: Issue JSON: ${JSON.stringify(issue, null, 2)}`);
+		const typeLabelsFound = pr.labels
+			?.map((label) => label.name)
+			.filter((labelName) => labelRegex.test(labelName));
+
+		const commentLines = [];
+
+		if (typeLabelsFound.length === 0) {
+			core.info(`>>> No type labels found`);
+
+			const prComments = await github.rest.issues.listComments({
+				owner,
+				repo,
+				pull_number: pullNumber,
+				per_page: 100,
+			});
+
+			const commentFound = prComments?.some((c) =>
+				c.body.includes(
+					'Please add one type label to categorize the purpose of this PR appropriately:',
+				),
+			);
+
+			if (commentFound) {
+				core.info(`>>> PR already has the type label comment.`);
+				core.info(`>>> Exiting gracefully! 👍`);
+				return;
+			}
+
+			// Add a comment line explaining that a type label needs to be added
+			commentLines.push(
+				'Please add one type label to categorize the purpose of this PR appropriately:',
+			);
+			commentLines.push(createEnumerationFromArray(typeLabels));
+		} else if (typeLabelsFound.length > 1) {
+			core.info(`>>> Multiple type labels found: ${typeLabelsFound.join(', ')}`);
+
+			// add a comment line explaining that only one type label is allowed
+			commentLines.push(`Multiple type labels found: ${typeLabelsFound.join(', ')}`);
+			commentLines.push(
+				'Only one is allowed. Please remove the extra type labels to ensure the PR is categorized correctly.',
+			);
+		} else {
+			core.info(`>>> Single type label found: ${typeLabelsFound[0]}`);
+			core.info(`>>> Exiting gracefully! 👍`);
+			return;
+		}
+
+		core.info(`>>> No type labels found.`);
+		core.info(`>>> Comment: \n"${commentLines.join('\n\n')}"`);
+
+		core.info(`>>> Creating explanatory comment on PR`);
+		await github.rest.issues.createComment({
+			owner,
+			repo,
+			issue_number: pullNumber,
+			body: commentLines.join('\n\n'),
+		});
+
+		core.setFailed('>>> Failing workflow to prevent merge without passing this!');
 	} catch (error) {
 		core.error(`>>> Workflow failed with: ${error.message}`);
 		core.setFailed(error.message);
